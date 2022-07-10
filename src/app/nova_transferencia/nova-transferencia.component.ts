565e7d1cde5ff4a7c89dcbd7adac7694c06645a0
samuel <samuelll455@gmail.com>
2022-07-10 14:27:28 -0300

    Page construction started

---
+++ src/app/nova_transferencia/nova-transferencia.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-nova_transferencia',
+    templateUrl:'./nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent{
+  transferir(){
+    console.log('EEEE');
+  }
+}
